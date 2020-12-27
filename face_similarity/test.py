02945cf5226c5ba169b83e27e3957ea8da5c6dbc
soorya_ram <soorya15012001@gmail.com>
2020-12-27 19:59:53 +0530

    Add files via upload

---
+++ face_similarity/test.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+from keras.models import load_model
+
+#image paths
+img1_path = 'data1/zico/Zico_0001.jpg'
+img2_path = 'data1/Aaron_Tippin/Aaron_Tippin_0001.jpg'
+
+#image arrays
+img_a = cv2.imread(img1_path).reshape(1,250,250,3)
+img_b = cv2.imread(img2_path).reshape(1,250,250,3)
+
+#class prediction using our trained model
+model = load_model('model3.h5')
+pred = model.predict({'image1': img_a, 'image2': img_b})
+if pred < 0.5:
+	class_ = 'No'
+else:
+	class_ = 'Yes'
+
+print("Image similarity metric as per our trained model: ", pred[0][0])
+print("Prediction - Are both images of the same person?", class_)
